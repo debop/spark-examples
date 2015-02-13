0b6055d7c0670e567668e9e30035d1297f51d786
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2015-02-13 18:42:35 +0900

    -. 기본 함수 추가.

--- src/test/scala/org/apache/spark/examples/AbstractSparkExample.scala
+++ src/test/scala/org/apache/spark/examples/AbstractSparkExample.scala
@@ -3,12 +3,24 @@ package org.apache.spark.examples
 import java.io.File
 
 import org.apache.commons.io.FileUtils
+import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FunSuite, Matchers}
 import org.slf4j.LoggerFactory
+
+import scala.util.Properties
 
 abstract class AbstractSparkExample extends FunSuite with Matchers with BeforeAndAfter with BeforeAndAfterAll {
 
   private val log = LoggerFactory.getLogger(getClass)
+
+  def getMaster: String = {
+    Properties.envOrElse("MASTER", "local")
+  }
+
+  def getSparkContext(appName: String): SparkContext = {
+    val conf = new SparkConf().setMaster(getMaster).setAppName(appName)
+    new SparkContext(conf)
+  }
 
   protected def deleteDirectory(path: String): Unit = {
     val dir = new File(path)
